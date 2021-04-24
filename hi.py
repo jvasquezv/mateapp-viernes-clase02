495cf602043ab2de3f523300235d94f87cd99fbd
Joel Vasquez <vasquezader@gmail.com>
2021-04-24 11:22:25 -0500

    trabajo

---
+++ hi.py
@@ -0,0 +1,60 @@
+print("Hola mundo")
+edad = 20
+print('Tengo', edad, 'años')
+
+base=10
+height=3
+
+area = base*height/2
+print('El area es: ',area)
+
+#Esto es un comentario
+#suma
+a = 5
+b = 6
+
+suma = a + b
+print('la suma es: ',suma)
+
+#resta
+resta = a -b 
+print('la resta es: ',resta)
+
+#multiplicacion 
+multiplicacion = a * b
+print('la multiplicacion es: ',multiplicacion)
+
+#division
+division = a/b
+print('la division es: ',division)
+
+#potencia
+potencia = a ** b
+print('la potencia es: ', potencia)
+
+#operaciones semi complejas
+#Realizar la siguiente operacion en Python ((4 + 5) * 3)/2
+
+x = 4
+y = 5
+w = 3
+z = 2
+
+operacion = ((x+y) * w )/z
+print(operacion)
+
+str1 = 'a'
+str2 = 'b'
+
+resultado = str1 + str2
+
+print(resultado)
+
+print(resultado * 5)
+
+caracteres = len("Hola mundo")
+print(caracteres)
+
+name = input('Cual es tu nombre?')
+print('Hola ', name)
+print('Hola '+ name)
